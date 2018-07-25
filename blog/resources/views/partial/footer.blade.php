d9dd9493d081bdfaa9ce5a43208fab1eba5a1dab
AlexSensei <antishadow15@gmai.com>
2018-07-25 10:00:21 +0200

    #AAKT-1 Code fix

--- blog/resources/views/partial/footer.blade.php
+++ blog/resources/views/partial/footer.blade.php
@@ -1,7 +1,5 @@
-
-
-    <footer class="page-footer font-small blue">
-        <div class="footer-copyright text-center py-3">© 2018 Copyright:
-            <p>Copyright of VivifyIdeas Intern.</p>
-        </div>
-    </footer>
+<footer class="page-footer font-small blue">
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+        <p>Copyright of VivifyIdeas Intern.</p>
+    </div>
+</footer>
